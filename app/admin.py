46a0d7e89d0d3232f35eadfa6fe0a8761211eaaf
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-14 11:36:43 +0300

    Rectification de template

--- app/admin.py
+++ app/admin.py
@@ -5,4 +5,6 @@ from . import models
 # Register your models here.
 @admin.register(models.Product)
 class ProductModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'discounted_price', 'category', 'product_image']
+    list_display = ['id', 'title', 'discounted_price', 'selling_price', 'category', 'product_image']
+    filter = ['title']
+    list_editable = ['discounted_price', 'selling_price']
