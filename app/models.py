f61442601fa8614dc096c88f9348f0f373820a6d
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-14 11:04:06 +0300

    Ajout model et migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+CATEGORY_CHOICES = (
+    ('CZ', 'Cheese'),
+    ('CR', 'Curd'),
+    ('GH', 'Ghee'),
+    ('IC', 'Ice-Creams'),
+    ('LS', 'Lassi'),
+    ('ML', 'Milk'),
+    ('MS', 'Milkshake'),
+    ('PN', 'Paneer'),
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.title
