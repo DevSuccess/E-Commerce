5569c13a06329584f7b2fc60c66cb298b90dba22
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-14 11:09:10 +0300

    Ajout produit

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+@admin.register(models.Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'discounted_price', 'category', 'product_image']
