b7ef8350f7ce476f92d93a53168f380bd6e900e4
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-15 08:08:47 +0300

    Debut

--- app/urls.py
+++ app/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(next_page='product:login'), name='logout'),
 
     path('password-reset/', auth_view.PasswordResetView.as_view(
-        # success_url=reverse_lazy('product:password-reset-done'),
+        # success_url=reverse_lazy('accounts:password_reset_done'),
         template_name='app/password_reset.html',
         form_class=forms.MyPasswordResetForm,
     ), name='password-reset'),
@@ -45,14 +45,15 @@ urlpatterns = [
     ), name='password-reset-done'),
     # 3:23:30
 
-    path('password-reset-complete/', auth_view.PasswordResetCompleteView.as_view(
-        template_name='app/password_reset_complete.html',
-    ), name='password-reset-complete'),
-
     path('password-reset-confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(
-        success_url=reverse_lazy('product:password-reset-complete'),
+        # success_url=reverse_lazy('product:password-reset-complete'),
+        # success_url=reverse_lazy('accounts:password_reset_complete'),
         template_name='app/password_reset_confirm.html',
         form_class=forms.MySetPasswordForm,
     ), name='password-reset-confirm'),
+
+    path('password-reset-complete/', auth_view.PasswordResetCompleteView.as_view(
+        template_name='app/password_reset_complete.html',
+    ), name='password-reset-complete'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
